3b9c795474a9e0fa5911a7749ec7f3a824d7703a
Cocoon <74912530+K-Junyyy@users.noreply.github.com>
2021-04-15 12:48:25 +0900

    Add files via upload

--- WEB2-JavaScript/colors.js
+++ WEB2-JavaScript/colors.js
@@ -1,18 +1,28 @@
+var Links = {
+    // 링크 색 바꾸기
+    SetColor: function (color) {
+        // var alist = document.querySelectorAll('a');
+        // for (var i = 0; i < alist.length; i++){
+        //     alist[i].style.color = color;
+        // }
+        $('a').css('color', color)
+    }
+    
+}
+
 var Body = {
+
     // 바디 글자색 바꾸기
-    SetColor:function(color){
-        document.querySelector('body').style.color = color;
+    SetColor: function (color) {
+        //document.querySelector('body').style.color = color;
+        $('body').css('color', color)
     },
     // 바디 배경색 바꾸기
-    SetBackgroundColor:function(color){
-    document.querySelector('body').style.backgroundColor = color;
-    },
-    // 링크 색 바꾸기
-    LinkSetColor:function(color){
-    var alist = document.querySelectorAll('a');
-    for(var i = 0; i < alist.length; i++)
-        alist[i].style.color = color;
+    SetBackgroundColor: function (color) {
+        //document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color)
     }
+
 }
 
 // 주간, 야간모드
@@ -21,13 +31,13 @@ function day_night_handler(self) {
     if (self.value == 'day') {
         Body.SetBackgroundColor('RGB(26,36,54)');
         Body.SetColor('white');
-        Body.LinkSetColor('powderblue')
+        Links.SetColor('powderblue')
         self.value = 'night';
     }
     else {
         Body.SetBackgroundColor('white');
         Body.SetColor('black');
-        Body.LinkSetColor('blue')
+        Links.SetColor('blue')
         self.value = 'day';
     }
 }
