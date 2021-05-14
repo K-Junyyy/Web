63bfdf5239b0c73484e2db7d87c94f8af362af87
Cocoon <74912530+K-Junyyy@users.noreply.github.com>
2021-05-14 21:51:53 +0900

    Delete WB2-Nodejs&MySQL directory

--- WB2-Nodejs&MySQL/password.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    id : 'cocoon',
-    password : '1234'
-}
--- WB2-Nodejs&MySQL/syntax/mpart.js
+++
@@ -1,9 +0,0 @@
-var M = {
-    v:'v',
-    f:function(){
-        console.log(this.v);
-    }
-}
-
-// 모듈 바깥에서 사용이 가능하도록 exports 함
-module.exports = M;
